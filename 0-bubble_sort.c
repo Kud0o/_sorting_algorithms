1a34eb91009b4193b003fefd28cb72cc7e291632
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-06-27 17:48:52 -0700

    Bubble Commit 2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,6 +7,11 @@
  * @size: Number of elements in @array
  */
 void bubble_sort(int *array, size_t size) {
+
+	if (array == null || size == 0)
+	{
+	return;
+	}
     size_t i, j, k;
     for (i = 0; i < size - 1; ++i) {
         for (j = 0; j < size - i - 1; ++j) {
