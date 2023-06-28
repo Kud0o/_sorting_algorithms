97a6d21b941f06e6c39eb9e9a258b53b348a559b
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-06-27 19:05:02 -0700

    Quick Commit 0

---
+++ 3-quick_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * quick_sort - quick sort an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void quick_sort(int *array, size_t size)
+{
+size_t i, j;
+int temp;
+
+if (array == NULL || size < 2)
+{
+return;
+}
+
+for (i = 0; i < size - 1; ++i)
+{
+for (j = 0; j < size - i - 1; ++j)
+{
+if (array[j] > array[j + 1])
+{
+/*Swap elements*/
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+print_array(array, size);
+}
+}
+}
+}
