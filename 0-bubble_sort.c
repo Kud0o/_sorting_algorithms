8cae773d2351bc57ad0ab80ec72f79d5491a8285
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-06-27 17:38:39 -0700

    first

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - bubble sort an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void bubble_sort(int *array, size_t size) {
+    size_t i, j;
+    for (i = 0; i < size - 1; ++i) {
+        for (j = 0; j < size - i - 1; ++j) {
+            if (array[j] > array[j + 1]) {
+                // Swap elements
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+        
+        for (size_t k = 0; k < size; ++k) {
+            if (k == 0)
+	    {
+	    	printf("%d", array[k]);
+	    }
+	    else
+	    {
+	    	printf(", %d ", array[k]);
+	    }
+        }
+        printf("\n");
+    }
+}
