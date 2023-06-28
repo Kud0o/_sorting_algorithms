fa83b3e3f46760abd890fb3cc10bddde962bc90d
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-06-27 17:51:51 -0700

    Bubble Commit 2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,11 +8,12 @@
  */
 void bubble_sort(int *array, size_t size) {
 
-	if (array == null || size == 0)
+	size_t i, j, k;
+
+	if (array == NULL || size == 0)
 	{
 	return;
 	}
-    size_t i, j, k;
     for (i = 0; i < size - 1; ++i) {
         for (j = 0; j < size - i - 1; ++j) {
             if (array[j] > array[j + 1]) {
