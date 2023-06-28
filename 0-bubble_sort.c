606de1a884db49f96c92c9fd99e79f459a597764
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-06-27 17:42:35 -0700

    Bubble Commit 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,18 +7,17 @@
  * @size: Number of elements in @array
  */
 void bubble_sort(int *array, size_t size) {
-    size_t i, j;
+    size_t i, j, k;
     for (i = 0; i < size - 1; ++i) {
         for (j = 0; j < size - i - 1; ++j) {
             if (array[j] > array[j + 1]) {
-                // Swap elements
                 int temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
             }
         }
         
-        for (size_t k = 0; k < size; ++k) {
+        for (k = 0; k < size; ++k) {
             if (k == 0)
 	    {
 	    	printf("%d", array[k]);
